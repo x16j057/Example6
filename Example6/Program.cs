1182dfb1c838f286edf0b02b5df0312210e61726
WIN_NT\x16j057 <x16j057@chiba-fjb.ac.jp>
2017-05-12 11:43:23 +0900

    スコア

---
+++ Example6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            double pai;
+            string name;
+
+            score = 25;
+            pai = 3.1415;
+            name = "taro";
+
+            Console.WriteLine("SCORE = {0}", score);
+            Console.WriteLine("AVE ={0}", pai);
+            Console.WriteLine("NAME={0}", name);
+        }
+    }
+}
